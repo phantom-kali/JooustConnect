b4423681665c6d03103abccb68f670c20f51aa8a
fideleliud <fideleliudclimax@gmail.com>
2024-08-01 02:02:56 +0300

    hardening

--- jooustconnect/urls.py
+++ jooustconnect/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bungu/', admin.site.urls),
     path('', include('socialapp.urls')),
 ]
 
